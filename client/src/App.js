7e69a7158738b91d0e1301a2d61151c12f53afdc
Jugal <jboro999@gmail.com>
2023-08-08 03:26:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,51 +16,45 @@ function App() {
           <Route
             path="/"
             element={
-       
+              <ProtectedRoute>
                 <Homepage />
-             
+              </ProtectedRoute>
             }
           />
           <Route
-            path="/get-item"
+            path="/items"
             element={
-            
+              <ProtectedRoute>
                 <ItemPage />
-
+              </ProtectedRoute>
             }
           />
           <Route
             path="/cart"
             element={
-            
+              <ProtectedRoute>
                 <CartPage />
-              
+              </ProtectedRoute>
             }
           />
           <Route
             path="/bills"
             element={
-           
+              <ProtectedRoute>
                 <BillsPage />
-          
+              </ProtectedRoute>
             }
           />
           <Route
             path="/customers"
             element={
-          
+              <ProtectedRoute>
                 <CutomerPage />
-           
+              </ProtectedRoute>
             }
           />
-          <Route
-            path="/login"
-            element={<Login />}
-          />
-          <Route
-            path="/register"
-            element={<Register />}
-          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
