ef41a90a8de904fd8a7b0ab3aabc0c2cf6a44f5c
Jugal <jboro999@gmail.com>
2023-08-08 02:57:56 +0530

    Update CartPage.js
    update auth id

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -93,7 +93,7 @@ const CartPage = () => {
         totalAmount: Number(
           Number(subTotal) + Number(((subTotal / 100) * 10).toFixed(2))
         ),
-        //userId: JSON.parse(localStorage.getItem("auth"))._id,
+        userId: JSON.parse(localStorage.getItem("auth"))._id,
       };
       // console.log(newObject);
       await axios.post(
